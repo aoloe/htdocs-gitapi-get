c124ae559725d35915e1135915d0af54337238ac
a.l.e <ale@ideale.ch>
2013-12-07 16:29:40 +0100

    start distingushing between libraries and fiels that are supposed to be accessed (api_* + update)

--- test/index.php
+++ test/index.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__) . '/simpletest/autorun.php');
 require_once('treepath.php');
 require_once('api_filelist.php');
 require_once('cache.php');
-require_once('api_fileraw.php');
+require_once('fileraw.php');
 require_once('gitapi.php');
 
 class AllTests extends TestSuite {
